663d9c733297cb5e7c06384e0272d3809fe26eab
Hadi Zorkot <hadizorkot@Hadis-MacBook-Pro.local>
2022-08-29 19:55:40 +0300

    Added environment Variables

--- spotistats/settings.py
+++ spotistats/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cm%ybn%8904t9na+e!uqutt$6*nxp177(!v=z!l7#o=^1vjssq"
+# SECRET_KEY = "django-insecure-cm%ybn%8904t9na+e!uqutt$6*nxp177(!v=z!l7#o=^1vjssq"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["spotistatsme.herokuapp.com"]
+ALLOWED_HOSTS = ["spotistatsme.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -118,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 # Default primary key field type
